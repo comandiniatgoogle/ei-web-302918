159833153577aa7281dd796cada78780a10e6060
comandiniatgoogle <comandini@google.com>
2022-05-16 00:34:41 +0100

    New text to verify if the script really works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (    
-    <h1><i>We are ready to go!</i></h1>
+    <h1><i>This application is implemented in ReactJs and run into Google AppEngine</i></h1>
   );
 }
 
