565624e09497bf8c442dbe3d573ef82b9350a620
comandiniatgoogle <comandini@google.com>
2022-05-16 00:50:37 +0100

    Login with our faces

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import './App.css';
 
 function App() {
-  return (    
-    <h1><i>This application is implemented in ReactJs and run into Google AppEngine</i></h1>
+  return ( 
+    <div>
+      <h1><i>This application is implemented in ReactJs and run into Google AppEngine</i></h1>
+      <table>
+        <tr>
+          <td colspan="2">THIS TWO GUYS ARE GENIUS</td>
+        </tr>
+        <tr>
+          <td>
+            <table>
+              <tr>
+                <td><img src="https://moma-teams-photos.corp.google.com/photos/comandini?sz=600&type=SECURITY&type=SILHOUETTE" /></td>
+              </tr>
+              <tr>
+                <td><b>DANIELE COMANDINI</b></td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <table>
+              <tr>
+                <td><img src="https://moma-teams-photos.corp.google.com/photos/smoroz?sz=600&type=SECURITY&type=SILHOUETTE" /></td>
+              </tr>
+              <tr>
+                <td><b>SERGEY MOROZ</b></td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </div>
   );
 }
 
