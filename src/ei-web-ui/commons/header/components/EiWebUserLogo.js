aeb38a17fb4a1713ec9330d60a5c934276879ada
comandiniatgoogle <comandini@google.com>
2022-05-18 15:52:29 +0100

    Logger user using Avatar

--- src/ei-web-ui/commons/header/components/EiWebUserLogo.js
+++ src/ei-web-ui/commons/header/components/EiWebUserLogo.js
@@ -1,14 +1,16 @@
-import './EiWebUserLogo.css';
+import { Avatar } from "@mui/material";
+import "./EiWebUserLogo.css";
 
 const EiWebUserLogo = (props) => {
   return (
-    <div align='center'>
-      <img
-        src={ `https://moma-teams-photos.corp.google.com/photos/${ props.userConnected.ldap }?sz=100&type=SECURITY&type=SILHOUETTE` }
-        alt={ props.userConnected.ldap }
+    <div align="center">
+      <Avatar
+        src={`https://moma-teams-photos.corp.google.com/photos/${props.userConnected.ldap}?sz=100&type=SECURITY&type=SILHOUETTE`}
+        alt={props.userConnected.ldap}
+        sx={{ width: 60, height: 60 }}
       />
       <br />
-      <span className="eiweb-userlogo-ldap">{ props.userConnected.name }</span>
+      <span className="eiweb-userlogo-ldap">{props.userConnected.name}</span>
     </div>
   );
 };
